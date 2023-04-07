5fa984a80523178924588c20aa6783ca4bff1f78
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2023-04-06 21:21:01 -0400

    added more of the components for routes, links is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./component/Navbar";
+import About from "./component/About";
+import Home from "./component/Home";
 
 function App() {
   return (
    <div>
    <Navbar />
+   <About />
+   <Home />
    </div>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons'
-import Hannah from "../assets/Hannah.png"
+import React from "react";
+import { FaBars, FaTimes } from "react-icons";
+import Hannah from "../assets/Hannah.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full h-[150px] flex justify-between items-center px-4 bg-[#333333] text-gray-500">
-        <div>
-            <img src={Hannah} alt="Logo" style={{width: "150px"}} />
-        </div>
-        <div>
-            <ul>
-                <l1>Home</l1>
-                <li>About</li>
-               <li>Applications</li>
-               <li>Contact</li>
-            </ul>
-        </div>
-    </div>
-  )
-}
+    <div className="z-50 fixed w-full h-[155px] flex justify-between items-center px-4 bg-[#333333] text-[#AD8F18]">
+      <div>
+        <a href="../components/home.jsx">
+          <img
+            className="rounded-full"
+            src={Hannah}
+            alt="Logo"
+            style={{ width: "155px" }}
+          />
+        </a>
+      </div>
 
-export default Navbar
+      <div>
+        <ul className="hidden md:flex">
+          <l1>
+            <Link to="About" smooth={true} duration={500}>
+              About
+            </Link>
+          </l1>
+          <l1>
+            <Link to="applications" smooth={true} duration={500}>
+                Applications
+            </Link>
+          </l1>
+
+          <li>About</li>
+          <li>Applications</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
